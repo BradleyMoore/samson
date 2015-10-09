9e9075c7be03c9c127f3a32b4b01d1bb807094e8
Bradley Moore <Bradley.Moore.TX@gmail.com>
2015-10-09 15:16:48 -0600

    fix basic bugs in my imports

---
+++ config.py
@@ -0,0 +1,17 @@
+DEBUG = True
+PROPAGATE_EXCEPTIONS = True
+SECRET_KEY = 'lkakjweoprruoq4jrp9qjp9f8qj34pf8j2q40qdj2348r0pd2jd4-2ud0ijiowjerpqj4awp4f'
+CSRF_ENABLED = True
+WTF_CSRF_ENABLED = True
+BASE_URL = 'https://api.groupme.com/v3'
+
+# samson group
+SAMSON_BOT_ID = 'da0afc4ccdf227de0dc95d3e7c'
+SAMSON_GROUP_ID = '14605695'
+
+# leadership group
+LEADERSHIP_BOT_ID = '57a735a6750a24d3981975f638'
+LEADERSHIP_GROUP_ID = '15197769'
+
+# samson app
+ACCESS_TOKEN = 'ae3a35b04f350133e58855655a9e4fa6'
