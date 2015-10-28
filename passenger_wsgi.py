02000797d21bb13ca9fb0c6debd371d6b8211929
Bradley Moore <Bradley.Moore.TX@gmail.com>
2015-10-28 14:24:07 -0600

    Change virtualenv from env to samson.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,8 @@
-import sys, os
+import os
+import sys
 
 
-virt_binary = '/home/flawles1/.env/env/bin/python'
+virt_binary = '/home/flawles1/.env/samson/bin/python'
 
 if sys.executable != virt_binary: os.execl(virt_binary, virt_binary, *sys.argv)
 sys.path.append(os.getcwd())
